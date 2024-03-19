6f4bad1356bbedf45cbcd71d20089bf994b2cdf7
DAVIDE LOTITO <davide.lotito@studenti.unicam.it>
2024-03-19 17:00:37 +0100

    Implementazione dei modelli: Carrello, Prodotto e creazione del modello Magazzino

--- ProgettoWebMobile/ProgettoWebMobile/Models/Carrello.cs
+++ ProgettoWebMobile/ProgettoWebMobile/Models/Carrello.cs
@@ -10,5 +10,44 @@ namespace ProgettoWebMobile.Modelli
 {
     public class Carrello
     {
+        public int IDCarrello { get; set; }
+        public List<Prodotto> ListaProdotti { get; set; }
+        public int QuantitaProdotti { get; set; }
+        public decimal PrezzoTotale { get; set; }
+        public DateTime DataCreazione { get; set; }
+        public DateTime DataUltimaModifica { get; set; }
+        public string StatoCarrello { get; set; }
+
+        public Carrello()
+        {
+            // Inizializzazione delle proprietà
+            ListaProdotti = new List<Prodotto>();
+            QuantitaProdotti = 0;
+            PrezzoTotale = 0;
+        }
+
+        // Metodo per calcolare il prezzo totale dei prodotti nel carrello
+        // Se la lista dei prodotti è nulla, restituisce 0
+        public Decimal CalcoloPrezzoTotale()
+        {
+            PrezzoTotale = 0; // Azzera il prezzo totale prima di calcolare
+            if (ListaProdotti != null)
+            {
+                foreach (var prodotto in ListaProdotti)
+                {
+                    PrezzoTotale += prodotto.Prezzo;
+                }
+            }
+            return PrezzoTotale;
+        }
+
+        // Metodo per ottenere la quantità totale dei prodotti nel carrello
+        // Se la lista dei prodotti è nulla, restituisce 0
+        public int QuantitaProdottiTotale()
+        {
+            if (ListaProdotti != null)
+                QuantitaProdotti = ListaProdotti.Count;
+            return QuantitaProdotti;
+        }
     }
 }
--- ProgettoWebMobile/ProgettoWebMobile/Models/Prodotto.cs
+++ ProgettoWebMobile/ProgettoWebMobile/Models/Prodotto.cs
@@ -10,5 +10,21 @@ namespace ProgettoWebMobile.Modelli
 {
     public class Prodotto
     {
+        public int IDProdotto { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public decimal Prezzo { get; set; }
+        public string Categoria { get; set; }
+        public string Marchio { get; set; }
+        public double Peso { get; set; }
+        public double Lunghezza { get; set; }
+        public double Larghezza { get; set; }
+        public double Altezza { get; set; }
+        public DateTime DataCreazione { get; set; }
+        public DateTime DataUltimaModifica { get; set; }
+        public string Taglia { get; set; }
+        public string Colore { get; set; }
+        public string Materiale { get; set; }
+
     }
 }
